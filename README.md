64f813fb527b2a3118deeab418c958ebf458dd17
Jason Clark Altmann <jason-clark.altmann@outlook.de>
2024-04-07 18:14:07 +0200

    added size changes and my personal links

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# text-paster
+# TextPaster
+
+TextPaster is a simple Rust application that provides a user interface for entering text and then "pasting" that text elsewhere.
+
+## Functionality
+
+The application provides a text field for users to enter text. After entering the text, users can click the "Paste" button to "paste" the entered text elsewhere. The application uses the `Enigo` library to simulate keyboard inputs and paste the text.
+
+The application waits three seconds before it "pastes" the text after the user clicks the "Paste" button. This gives the user time to set the focus to the window or field where the text should be pasted.
+
+The application also has a footer with the author's name and a link to the application's GitHub repo.
+
+## Usage
+
+To use the application, simply run the program and enter the text you want to "paste". Then click the "Paste" button and set the focus to the window or field where the text should be pasted within three seconds.
+
+## Installation
+
+To install the application, clone the repository and build the program with `cargo build`. You will need Rust and Cargo on your system to build the program.
+
+```bash
+git clone https://github.com/JasonClarkAltmann/text-paster.git
+cd text-paster
+cargo build --release
+```
+
+The built program will then be in the `target/release` directory.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 #![windows_subsystem = "windows"]
 
-
-use eframe::{egui, App, Frame, CreationContext};
+use eframe::{egui, App, CreationContext, Frame};
 use enigo::{Enigo, KeyboardControllable};
 use std::{thread, time};
-
 
 struct TextPaster {
     text: String,
@@ -23,20 +21,34 @@ impl TextPaster {
 impl App for TextPaster {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
         ctx.set_pixels_per_point(1.2);
-    
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Text Paster");
             ui.separator();
-    
+
             egui::ScrollArea::vertical().show(ui, |ui| {
                 ui.vertical_centered(|ui| {
-                    ui.add(egui::TextEdit::multiline(&mut self.text));
+                    ui.add(egui::TextEdit::multiline(&mut self.text).desired_width(350.0));
                     ui.add_space(10.0);
 
                     if ui.button("Paste").clicked() {
                         thread::sleep(time::Duration::from_secs(3));
                         self.enigo.key_sequence(&self.text);
                     }
+                    ui.add_space(10.0);
+                });
+            });
+
+            egui::TopBottomPanel::bottom("footer").show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Jason Clark Altmann");
+                    ui.add_space(5.0);
+
+                    use egui::special_emojis::GITHUB;
+                    ui.hyperlink_to(
+                        format!("{GITHUB} GitHub"),
+                        "https://github.com/JasonClarkAltmann/text-paster",
+                    );
                 });
             });
         });
@@ -45,8 +57,15 @@ impl App for TextPaster {
 
 fn main() {
     let app = TextPaster::new();
-    let native_options = eframe::NativeOptions::default();
-    let result = eframe::run_native("Text Paster", native_options, Box::new(move |_ctx: &CreationContext<'_>| Box::new(app)));
+    let native_options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(500.0, 200.0)),
+        ..Default::default()
+    };
+    let result = eframe::run_native(
+        "Text Paster",
+        native_options,
+        Box::new(move |_ctx: &CreationContext<'_>| Box::new(app)),
+    );
 
     result.unwrap_or_else(|e| eprintln!("Fehler beim Ausführen der Anwendung: {:?}", e));
 }
